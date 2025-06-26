c5da67a9cd5a77573b601666b223af25394f045d
Seba_Fon <sebastian.fonseca@correo.ucu.edu.uy>
2025-06-26 20:51:43 -0300

    Se agrego varios pacientes sin informe odontologico

--- sistemasobligatorio/src/main/java/sistemasobligatorio/Main.java
+++ sistemasobligatorio/src/main/java/sistemasobligatorio/Main.java
@@ -1,18 +1,8 @@
 package sistemasobligatorio;
 
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.Comparator;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 class Main {
     public static void main(String[] args) {
-        Clinica clinica = new Clinica("sistemasobligatorio\\src\\main\\java\\sistemasobligatorio\\pacientesDelDia.txt",
+        Clinica clinica = new Clinica("sistemasobligatorio\\src\\main\\java\\sistemasobligatorio\\PacienteSinInforme.txt",
                 "Dr. House",
                 "Enfermero Stark", "Pam Beesly");
 
